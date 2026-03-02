e227cf5d86056ff083dfcb2c853bc202a7464782
karcher <karchernic@gmail.com>
2026-03-02 16:40:22 +0100

    adding test_run_concurrent.sh

---
+++ test_run_concurent.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+base_outdir="/scratch/karcher/barflow_output"
+project_dir="/g/typas/Personal_Folders/Nic/barflow"
+
+if [[ "$#" -lt 1 ]]; then
+    echo "Error: RUN_ID is required as the first argument. Just needs to be a unique identifier for this test run (e.g. 'test_run_1')"
+    echo "Usage: $0 <RUN_ID>"
+    exit 1
+fi
+
+RUN_ID="$1"
+
+echo "Starting test run with RUN_ID: ${RUN_ID}"
+
+mkdir -p "${base_outdir}/${RUN_ID}"
+
+nextflow run "${project_dir}/main.nf" \
+  -profile slurm \
+  --test_run true \
+  --samplesheet "${project_dir}/test_data/test_samplesheet.csv" \
+  --good_barcodes_csv "${project_dir}/test_data/good_barcodes_with_locustag.csv" \
+  --outdir "${base_outdir}/${RUN_ID}" \
+  --comparisons "${project_dir}/test_data/comparisons.json" \
+  --singularity true \
+  --conda false \
+  -work-dir "${base_outdir}/${RUN_ID}/work"
